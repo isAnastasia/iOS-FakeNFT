cdccb22093c923db6526afcbf530c4cc46c74a44
Dmitry <otlichnik24@yandex.ru>
2024-05-24 19:16:54 +0300

    started making Profile

---
+++ FakeNFT/Scenes /Profile/Components/Labels.swift
@@ -0,0 +1,20 @@
+//
+//  Labels.swift
+//  FakeNFT
+//
+//  Created by Dmitry Dmitry on 24.5.2024.
+//
+
+import UIKit
+
+class Labels: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
