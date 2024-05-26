7b2622f53dda280f7a91f47c57f64b13c956d92c
isAnastasia <nasty04012003@gmail.com>
2024-05-26 13:30:45 +0300

    baseURL changed

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,6 +1,7 @@
 enum RequestConstants {
     #warning("insert your baseUrl <id>.mockapi.io")
-    static let baseURL = "https://64858e8ba795d24810b71189.mockapi.io/"
+    //static let baseURL = "https://64858e8ba795d24810b71189.mockapi.io/"
+    static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net/"
     
     static let acceptKey = "Accept"
     static let acceptValue = "application/json"
